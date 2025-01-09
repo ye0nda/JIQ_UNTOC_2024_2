b9723ccb5667b577c06a87d5ad8d796c8955e82d
ye0nda <yeonda@pusan.ac.kr>
2025-01-09 17:11:32 +0900

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
 from database import FolderBase, FileBase, UserBase, QuizBase, RetryBase
 import datetime
+
+# Retry 테이블
 class Retry(RetryBase):
     __tablename__ = "retry_attempts"
     id = Column(Integer, primary_key=True, index=True)
@@ -9,7 +10,7 @@ class Retry(RetryBase):
     quiz_id = Column(Integer, ForeignKey("quizzes.id"))
     is_correct = Column(Boolean, default=False)
     attempted_at = Column(DateTime, default=datetime.datetime.utcnow)
-    
+
 # Folder 테이블
 class Folder(FolderBase):
     __tablename__ = "folder"
@@ -29,16 +30,10 @@ class User(UserBase):
     username = Column(String(255), unique=True, nullable=False)  # VARCHAR(255)
     email = Column(String(255), unique=True, nullable=False)  # VARCHAR(255)
     hashed_password = Column(String(255), nullable=False)  # VARCHAR(255)
-    retry_attempts = relationship("RetryAttempt", back_populates="user")
 
+# Quiz 테이블
 class Quiz(QuizBase):
     __tablename__ = "quizzes"
     id = Column(Integer, primary_key=True, index=True)
     question = Column(String(255), nullable=False)  # VARCHAR(255)
     answer = Column(String(255), nullable=False)  # VARCHAR(255)
-    retry_attempts = relationship("RetryAttempt", back_populates="quiz")
-
-
-
-    user = relationship("User", back_populates="retries")
-    quiz = relationship("Quiz", back_populates="retries")
